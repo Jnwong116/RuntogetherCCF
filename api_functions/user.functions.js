a8a3b02f258a9bfb423042f54309e70c438e218f
Jordan <jnwong116@gmail.com>
2023-02-27 00:14:38 -0500

    Created all post requests

--- api_functions/user.functions.js
+++ api_functions/user.functions.js
@@ -27,4 +27,81 @@ const deleteUser = (username) => {
   return User.findOneAndDelete({ name: username });
 };
 
-module.exports = { createUser, getAllUsers, getUser, deleteUser };
+const updateVision = async (username, vision) => {
+  const user = await User.findOne({ name: username });
+
+  // Checks if a user exists
+  if (user === null) {
+    throw "User not found";
+  }
+
+  user.vision = vision;
+
+  user.save();
+  return user;
+};
+
+const uploadNominationLink = async (username, nominationLink) => {
+  const user = await User.findOne({ name: username });
+
+  // Checks if a user exists
+  if (user === null) {
+    throw "User not found";
+  }
+
+  user.nominationLink = nominationLink;
+  user.save();
+  return user;
+};
+
+const updatePosition = async (username, position) => {
+  const user = await User.findOne({ name: username });
+
+  // Checks if a user exists
+  if (user === null) {
+    throw "User not found";
+  }
+
+  user.position = position;
+  user.save();
+  return user;
+};
+
+const updateNominations = async (username, nominator, nomination) => {
+  const user = await User.findOne({ name: username });
+
+  // Checks if a user exists
+  if (user === null) {
+    throw "User not found";
+  }
+
+  // Finds correct nomination
+  for (let i = 0; i < user.nominations.length; i++) {
+    if (user.nominations[i].nominator === nominator) {
+      user.nominations[i].nomination = nomination;
+      user.save();
+      return user;
+    }
+  }
+
+  // If no nomination is found, create a new one
+  const newNomination = {
+    nominator: nominator,
+    nomination: nomination,
+  };
+
+  user.nominations.push(newNomination);
+  user.save();
+  return user;
+};
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUser,
+  deleteUser,
+  updateVision,
+  uploadNominationLink,
+  updatePosition,
+  updateNominations,
+};
