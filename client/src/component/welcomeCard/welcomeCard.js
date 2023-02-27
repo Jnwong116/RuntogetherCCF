30b2551fc7c664406c20882261263f740a8ecb3d
Jordan <jnwong116@gmail.com>
2023-02-27 17:34:57 -0500

    Updated the messages for welcome card based on if user was logged in or not

--- client/src/component/welcomeCard/welcomeCard.js
+++ client/src/component/welcomeCard/welcomeCard.js
@@ -4,18 +4,27 @@ import WhiteContainer from "../whiteContainer/whiteContainer";
 const NOT_REGISTERED_MESSAGE =
   "You are not registered in the running for any positions yet. If you would like to do so, click the button below the position that you want to run for. If you need to upload nominations for someone, please ask them to send you a “request nominations” link.";
 
+const NOT_LOGGED_IN_MESSAGE = " please log into Facebook.";
+
 const WelcomeCard = ({ user }) => {
-  const runningForPosition = user.position !== null;
+  const runningForPosition = user.position !== undefined;
+  const loggedIn = user.name !== undefined;
+  console.log(user);
 
   return (
     <WhiteContainer maxWidth="600px" margin="1.5rem auto" textAlign="center">
       <Typography variant="h3" color="textPrimary" paddingBottom="0.5rem">
-        Welcome, <strong>{user.name}</strong>.
+        Welcome,
+        {loggedIn ? <strong> {user.name}</strong> : NOT_LOGGED_IN_MESSAGE}
       </Typography>
-      {runningForPosition ? (
-        "" // TODO: Add registered component with buttons
+      {loggedIn ? (
+        runningForPosition ? (
+          "" // TODO: Add registered component with buttons
+        ) : (
+          <Typography color="textPrimary">{NOT_REGISTERED_MESSAGE}</Typography>
+        )
       ) : (
-        <Typography color="textPrimary">{NOT_REGISTERED_MESSAGE}</Typography>
+        ""
       )}
     </WhiteContainer>
   );
