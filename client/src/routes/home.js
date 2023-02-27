8d648549266ed197a68a10a52f97a90ae3277698
Jordan <jnwong116@gmail.com>
2023-02-27 16:33:57 -0500

    Deleted mock user and changed home page to react class

--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -5,43 +5,39 @@ import { Box } from "@mui/system";
 import WelcomeCard from "../component/welcomeCard/welcomeCard";
 import PersonList from "../component/personList/personList";
 import { COMMITTEE_POSITIONS, MINISTRY_POSITIONS } from "../actions/positions";
+import FacebookLoginButton from "../component/facebookLoginButton/facebookLoginButton";
 
 const ELECTIONS_HEADER_TEXT = "2023 - 2024 Leadership Elections";
 
-const mockUser = {
-  name: "JdogDragon the First",
-  // position: "Chair",
-  position: null,
-  visionName: "CCF Bears",
-  visionFile: "CCFBears.pdf",
-  nominations: [
-    {
-      name: "Marian the Librarian",
-      nomination:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-  ],
-};
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+    };
+  }
 
-const Home = () => {
-  return (
-    <>
-      <WelcomeCard user={mockUser} />
-      <Divider sx={{ bgcolor: "secondary.light", margin: "2.5rem" }} />
-      <Typography variant="h2" color="textPrimary" textAlign="center">
-        {ELECTIONS_HEADER_TEXT}
-      </Typography>
-      <Box
-        display="flex"
-        justifyContent="center"
-        flexWrap="wrap"
-        marginTop="1rem"
-      >
-        <PersonList title="Committee" positions={COMMITTEE_POSITIONS} />
-        <PersonList title="IGs & Ministries" positions={MINISTRY_POSITIONS} />
-      </Box>
-    </>
-  );
-};
+  render() {
+    return (
+      <>
+        <FacebookLoginButton parent={this} />
+        <WelcomeCard user={this.state.user} />
+        <Divider sx={{ bgcolor: "secondary.light", margin: "2.5rem" }} />
+        <Typography variant="h2" color="textPrimary" textAlign="center">
+          {ELECTIONS_HEADER_TEXT}
+        </Typography>
+        <Box
+          display="flex"
+          justifyContent="center"
+          flexWrap="wrap"
+          marginTop="1rem"
+        >
+          <PersonList title="Committee" positions={COMMITTEE_POSITIONS} />
+          <PersonList title="IGs & Ministries" positions={MINISTRY_POSITIONS} />
+        </Box>
+      </>
+    );
+  }
+}
 
 export default Home;
