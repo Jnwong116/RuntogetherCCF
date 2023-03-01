2ca5862ea250afc96e99b161706966a3c34e3097
Marian Wang <marian.mian.wang@gmail.com>
2023-02-28 23:52:23 -0800

    Update user model schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const statusSchema = newSchema({
+  status: {
+    type: String,
+    enum: ["WAITING", "CONSIDERING", "DECIDED"],
+  },
+});
+
 const nominationSchema = new Schema({
   nominator: { type: String, required: true },
   nomination: { type: String, required: true },
@@ -10,9 +17,11 @@ const userSchema = new Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
   picture: { type: String, required: true },
-  position: { type: String, required: true },
+  positions: { type: [String], required: true, default: [] },
+  status: { type: statusSchema, required: true, default: "WAITING" },
   nominations: { type: [nominationSchema], required: true, default: [] },
-  vision: { type: String, default: "" },
+  visionName: { type: String, default: "" },
+  visionLink: { type: String, default: "" },
   nominationLink: { type: String },
 });
 
