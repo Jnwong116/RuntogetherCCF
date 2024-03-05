7263cac47549a00a21762fe6283997f3cec858d1
Marian Wang <marian.mian.wang@gmail.com>
2024-03-05 12:16:44 -0500

    add client id

--- client/src/config.js
+++ client/src/config.js
@@ -1,17 +1,14 @@
 const port = 3000;
 
-const google_client_id = process.env.GOOGLE_CLIENT_ID;
-console.log(google_client_id)
-
 const dev = {
   port,
   api_host: `http://localhost:${port}`,
-  google_client_id: google_client_id,
+  google_client_id: "583586195557-he3bnhr51lmj6leqh31p16itneghf4bp.apps.googleusercontent.com",
 };
 
 const prod = {
   api_host: "https://runtogetherccf-8qe6.onrender.com",
-  google_client_id: google_client_id,
+  google_client_id: "583586195557-he3bnhr51lmj6leqh31p16itneghf4bp.apps.googleusercontent.com",
 };
 
 module.exports = prod;
