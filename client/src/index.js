a2183f97e58a340988c719eb42c832794f0cde73
Marian Wang <marian.mian.wang@gmail.com>
2024-03-06 00:14:39 -0500

    play around w index.js

--- client/src/index.js
+++ client/src/index.js
@@ -35,10 +35,12 @@ let router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <RouterProvider router={router} />
     <ToastContainer limit={1} />
-  </React.StrictMode>
+  </>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
